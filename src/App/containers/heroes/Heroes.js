6f303938cc608adc147b84ae531c0073da7e1449
Dhananjay Mane <dhananjay.mane@mobisoftinfotech.com>
2019-03-13 21:58:10 +0530

    Added new-delete hero functionality

--- src/App/containers/heroes/Heroes.js
+++ src/App/containers/heroes/Heroes.js
@@ -5,20 +5,55 @@ import { Link } from "react-router-dom";
 import * as actions from '../../core/actions';
 
 class HeroesContainer extends Component {
+    state = {
+        name: ''
+    }
 
     componentDidMount() {
         this.props.loadHeroes();
+    }
+
+    handleNameChange = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    handleAddNewHero = () => {
+        const lastMemberId = this.props.heroes.result.length ? this.props.heroes.result[this.props.heroes.result.length - 1].id : 0;
+        const newHero = {
+            id: lastMemberId + 1,
+            name: this.state.name
+        }
+        const heroes = [...this.props.heroes.result, newHero];
+        this.props.updateHeroes(heroes);
+        this.setState({
+            name: ''
+        })
+    }
+
+    handleDeleteHero = (id) => {
+        const heroes = [...this.props.heroes.result];
+        const delteHeroIndex = heroes.findIndex(hero => hero.id === id);
+        heroes.splice(delteHeroIndex, 1);
+        debugger
+        this.props.updateHeroes(heroes);
     }
 
     render() {
         return (
             <div>
                 <h1>Heroes</h1>
+                <div>
+                    <input type="text" value={this.state.name} onChange={this.handleNameChange} />
+                    <button onClick={this.handleAddNewHero}>Add</button>
+                </div>
                 {this.props.heroes.result ? this.props.heroes.result.map(hero => (
                     <div key={hero.id}>
                         <Link to={`heroes/${hero.id}`}>
                             <span className="badge">{hero.id}</span> {hero.name}
                         </Link>
+                        <button classNam="button-btn" title="delete hero" onClick={() => this.handleDeleteHero(hero.id)}>x</button>
                     </div>
                 )) : null}
             </div>
@@ -30,6 +65,9 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     return {
         loadHeroes: () => {
             dispatch(actions.loadHeroes())
+        },
+        updateHeroes: (heroes) => {
+            dispatch(actions.updateHeroes(heroes))
         }
     }
 }
